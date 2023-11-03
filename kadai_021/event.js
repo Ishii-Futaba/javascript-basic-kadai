bbacd8159b05869632676deb7e2d6f9b632793b5
Ishii-Futaba <148844312+Ishii-Futaba@users.noreply.github.com>
2023-11-03 15:21:40 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,8 @@
+const text = document.getElementById('text');
+
+btn.addEventListener('click', () => {
+	setTimeout(() => {
+		//2秒（2000ミリ秒）の待ち時間を設定し、非同期処理を実行する
+		text.textContent = "ボタンをクリックしました";
+	}, 2000);
+});
